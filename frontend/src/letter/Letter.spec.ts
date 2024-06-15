65f002884a6955264e971360857683eb933b4121
kerabite <10791585+kerabite@users.noreply.github.com>
2024-06-15 11:54:50 -0500

    Added linked letters to Letter
    Signed-off-by: kerabite <10791585+kerabite@users.noreply.github.com>

--- frontend/src/letter/Letter.spec.ts
+++ frontend/src/letter/Letter.spec.ts
@@ -1,73 +1,120 @@
-import {Letter} from "./Letter.ts";
+import {Letter} from './Letter.ts';
+import {expect} from "vitest";
 
 describe('Letter.increase() tests', () => {
-    test.each([
+    it.each([
         ['A', 1, 'B'],
         ['B', 1, 'C'],
         ['A', 2, 'C']
-    ])('increasing %s by %i becomes %s', (letter, amount, expectedLetter) => {
-        const letterA = new Letter(letter);
+    ])('should increase %s by %i to become %s', (letter, amount, expectedLetter) => {
+        const actualLetter = new Letter(letter, []);
+        actualLetter.increase(amount);
 
-        expect(letterA.increase(amount)).toStrictEqual(new Letter(expectedLetter));
+        expect(actualLetter).toStrictEqual(new Letter(expectedLetter, []));
     })
     describe('wrapping tests', () => {
-        test.each([
+        it.each([
             ['Z', 1, 'A'],
             ['Y', 2, 'A'],
             ['Z', 2, 'B'],
-        ])('increasing %s by %i becomes %s', (letter, amount, expectedLetter) => {
-            const letterA = new Letter(letter);
+        ])('should increase %s by %i to become %s', (letter, amount, expectedLetter) => {
+            const actualLetter = new Letter(letter, []);
+            actualLetter.increase(amount);
 
-            expect(letterA.increase(amount)).toStrictEqual(new Letter(expectedLetter));
+            expect(actualLetter).toStrictEqual(new Letter(expectedLetter, []));
         })
     })
+    describe('linked letters tests', () => {
+        it.each([
+            ['b', 1, 'c'],
+            ['b', 2, 'd'],
+            ['c', 1, 'd'],
+            ['x', 1, 'y']
+        ])('should increase one linked letter %s by %i when increased same amount', (letter, amount, expected) => {
+            const linkedLetter = new Letter(letter, []);
+            const actualLetter = new Letter('a', [linkedLetter]);
+
+            actualLetter.increase(amount);
+
+            expect(linkedLetter.letter).toStrictEqual(expected);
+        });
+        // TODO: not the best test, should make linked letters have different
+        it.each([
+            ['b', 1, 'c'],
+            ['b', 2, 'd'],
+            ['c', 1, 'd'],
+            ['x', 1, 'y']
+        ])('should increase multiple linked letter %s by %i when increased same amount', (letter, amount, expected) => {
+            const linkedLetter1 = new Letter(letter, []);
+            const linkedLetter2 = new Letter(letter, []);
+            const actualLetter = new Letter('a', [linkedLetter1, linkedLetter2]);
+
+            actualLetter.increase(amount);
+
+            expect(linkedLetter1.letter).toStrictEqual(expected);
+            expect(linkedLetter2.letter).toStrictEqual(expected);
+        });
+        it('should update linkedLetters of linked letters', () => {
+            // TODO
+        });
+    });
 });
 
+/**
+ * TODO: these are basically just copies of increase() with swapped expected.
+ * maybe just test each function by passing them in as cases
+ */
 describe('Letter.decrease() tests', () => {
-    test.each([
+    it.each([
         ['B', 1, 'A'],
         ['C', 1, 'B'],
         ['C', 2, 'A']
-    ])('decreasing %s by %i becomes %s', (letter, amount, expectedLetter) => {
-        const letterA = new Letter(letter);
+    ])('should decrease %s by %i to become %s', (letter, amount, expectedLetter) => {
+        const actualLetter = new Letter(letter, []);
+        actualLetter.decrease(amount);
 
-        expect(letterA.decrease(amount)).toStrictEqual(new Letter(expectedLetter));
+        expect(actualLetter).toStrictEqual(new Letter(expectedLetter, []));
     })
     describe('wrapping tests', () => {
-        test.each([
+        it.each([
             ['A', 1, 'Z'],
             ['A', 2, 'Y'],
             ['B', 2, 'Z'],
-        ])('decreasing %s by %i becomes %s', (letter, amount, expectedLetter) => {
-            const letterA = new Letter(letter);
+        ])('should decrease %s by %i to become %s', (letter, amount, expectedLetter) => {
+            const actualLetter = new Letter(letter, []);
+            actualLetter.decrease(amount);
 
-            expect(letterA.decrease(amount)).toStrictEqual(new Letter(expectedLetter));
+            expect(actualLetter).toStrictEqual(new Letter(expectedLetter, []));
         })
     })
 });
 
 describe('Letter constructor tests', () => {
     describe('single letter tests', () => {
-        test.each('abcdefghijklmnopqrstuvwxyz'.split(''))('single char %s works', (letter) => {
-            expect(new Letter(letter).letter).toStrictEqual(letter);
+        it.each('abcdefghijklmnopqrstuvwxyz'.split(''))('single char %s works', (letter) => {
+            expect(new Letter(letter, []).letter).toStrictEqual(letter);
         })
-        test.each('abcdefghijklmnopqrstuvwxyz'.toUpperCase().split(''))('upper case char %s works', (letter) => {
-            expect(new Letter(letter).letter).toStrictEqual(letter.toLowerCase());
+        it.each('abcdefghijklmnopqrstuvwxyz'.toUpperCase().split(''))('upper case char %s works', (letter) => {
+            expect(new Letter(letter, []).letter).toStrictEqual(letter.toLowerCase());
         })
-        test.each('1234567890!@#$%^&*()-=_+[]{}|;:,.<>/?\\'.split(''))('non-alphabetic char %s does not work', (invalidLetter) => {
-            expect(() => { new Letter(invalidLetter) }).toThrowError();
+        it.each('1234567890!@#$%^&*()-=_+[]{}|;:,.<>/?\\'.split(''))('non-alphabetic char %s does not work', (invalidLetter) => {
+            expect(() => {
+                new Letter(invalidLetter, [])
+            }).toThrowError();
         })
     })
     describe('multiple letter tests', () => {
-        test.each([
+        it.each([
             'ab',
             'bc',
             'ad',
             'abc',
             'abd',
             'jduw'
-        ])('%s should not work', (invalidLetter) => {
-            expect(() => { new Letter(invalidLetter) }).toThrowError();
+        ])('%s should not work', (multipleLetters) => {
+            expect(() => {
+                new Letter(multipleLetters, [])
+            }).toThrowError();
         })
     })
 })
--- frontend/src/letter/Letter.ts
+++ frontend/src/letter/Letter.ts
@@ -1,6 +1,6 @@
 export class Letter {
     // TODO: look into making _letter a special length 1 string type
-    constructor(private readonly _letter: string) {
+    constructor(private _letter: string, private _linkedLetters: Letter[]) {
         if (_letter.length !== 1) {
             throw new TypeError('Letter must be one character');
         }
@@ -12,12 +12,17 @@ export class Letter {
 
     // public methods
 
-    get letter(): string { return this._letter; }
+    get letter(): string {
+        return this._letter;
+    }
 
     increase(amount: number) {
         const currentCharCode = this._letter.charCodeAt(0);
 
-        return new Letter(String.fromCharCode(this.getResultingCharCode(currentCharCode + amount)));
+        this._letter = String.fromCharCode(this.getResultingCharCode(currentCharCode + amount));
+        for (const linkedLetter of this._linkedLetters) {
+            linkedLetter.increase(amount);
+        }
     }
 
     decrease(amount: number) {

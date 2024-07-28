3f0c540383b6f24ec94a4bcedb4657bf05c7c26d
Julian Kim <kimjulian@grinnell.edu>
2024-07-28 17:08:25 -0500

    Added and used eslint-config-prettier

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,11 +2,13 @@
 
 import eslint from '@eslint/js'
 import tsEslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
 
 export default tsEslint.config(
   eslint.configs.recommended,
   ...tsEslint.configs.strict,
   ...tsEslint.configs.stylistic,
+  prettierConfig,
   {
     ignores: ['**/dist/', '**/*_old*(.*)']
   }

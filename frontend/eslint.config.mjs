3d613fabec5b3b5218121f164e19264a283cb218
Julian Kim <kimjulian@grinnell.edu>
2024-07-30 21:22:41 -0500

    Fixed Vitest not being defined due to ESLint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,12 +3,28 @@
 import eslint from '@eslint/js'
 import tsEslint from 'typescript-eslint'
 import prettierConfig from 'eslint-config-prettier'
+import vitest from 'eslint-plugin-vitest'
 
 export default tsEslint.config(
   eslint.configs.recommended,
   ...tsEslint.configs.strict,
   ...tsEslint.configs.stylistic,
   prettierConfig,
+  {
+    files: [
+      'src/**/*.spec.ts',
+      'src/**/*.spec.tsx',
+      'testing/**/*.ts',
+      'testing/**/*.spec.ts',
+      'testing/**/*.spec.tsx'
+    ], // or any other pattern
+    plugins: {
+      vitest
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
   {
     ignores: ['**/dist/**', '**/*_old*(.*)', '**/*_old/**']
   }

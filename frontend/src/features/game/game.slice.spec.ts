634288b760f366acfb825394c9bb8ea936c2ea0c
Julian Kim <kimjulian@grinnell.edu>
2024-07-30 21:56:27 -0500

    Added clicks state to GameState

--- frontend/src/features/game/game.slice.spec.ts
+++ frontend/src/features/game/game.slice.spec.ts
@@ -1,68 +1,71 @@
-import gameReducer, {decrement, disableGeneration, enableGeneration, GameState, increment} from "./game.slice.ts";
+import gameReducer, {
+    decrement,
+    disableGeneration,
+    enableGeneration,
+    GameState,
+    increment,
+    initialGameState,
+} from './game.slice.ts';
 
-describe('game slice tests', () => {
-    const testState: GameState = {
-        letters: ["J", "U", "L", "I", "N", "K"],
-        canGenerateHints: [true, true, true, true, true, true]
-    };
+describe.concurrent('game slice tests', () => {
+    const testState: GameState = initialGameState;
     const enableTestState: GameState = {
         ...testState,
-        canGenerateHints: [false, false, false, false, false, false]
+        canGenerateHints: [false, false, false, false, false, false],
     };
 
     // TODO: consider breaking into test suites, haven't done yet just because i'm not sure how concurrent works
     it('should return the initial state', async () => {
-        expect(gameReducer(undefined, {type: 'unknown'})).toStrictEqual({
-            letters: ["J", "U", "L", "I", "N", "K"],
-            canGenerateHints: [true, true, true, true, true, true]
+        expect(gameReducer(undefined, { type: 'unknown' })).toMatchObject({
+            letters: ['J', 'U', 'L', 'I', 'N', 'K'],
+            canGenerateHints: [true, true, true, true, true, true],
+            clicks: 0,
         });
     });
     it('should increment 2 letters at passed indices', async () => {
-        expect(gameReducer(testState, increment(0))).toStrictEqual({
-            ...testState,
-            letters: ["K", "V", "L", "I", "N", "K"],
+        expect(gameReducer(testState, increment(0))).toMatchObject({
+            letters: ['K', 'V', 'L', 'I', 'N', 'K'],
         });
     });
     it('should increment 3 letters at passed indices', async () => {
-        expect(gameReducer(testState, increment(2))).toStrictEqual({
-            ...testState,
-            letters: ["J", "V", "M", "J", "N", "K"],
+        expect(gameReducer(testState, increment(2))).toMatchObject({
+            letters: ['J', 'V', 'M', 'J', 'N', 'K'],
         });
     });
     it('should decrement 2 letters at passed indices', async () => {
-        expect(gameReducer(testState, decrement(5))).toStrictEqual({
-            ...testState,
-            letters: ["J", "U", "L", "I", "M", "J"],
+        expect(gameReducer(testState, decrement(5))).toMatchObject({
+            letters: ['J', 'U', 'L', 'I', 'M', 'J'],
         });
     });
     it('should decrement 3 letters at passed indices', async () => {
-        expect(gameReducer(testState, decrement(3))).toStrictEqual({
-            ...testState,
-            letters: ["J", "U", "K", "H", "M", "K"],
+        expect(gameReducer(testState, decrement(3))).toMatchObject({
+            letters: ['J', 'U', 'K', 'H', 'M', 'K'],
         });
     });
     it('should enable hint generation for 2 letters at passed indices', async () => {
-        expect(gameReducer(enableTestState, enableGeneration([0, 1]))).toStrictEqual({
-            ...enableTestState,
-            canGenerateHints: [true, true, false, false, false, false]
+        expect(
+            gameReducer(enableTestState, enableGeneration([0, 1]))
+        ).toMatchObject({
+            canGenerateHints: [true, true, false, false, false, false],
         });
     });
     it('should enable hint generation for 3 letters at passed indices', async () => {
-        expect(gameReducer(enableTestState, enableGeneration([3, 4, 5]))).toStrictEqual({
-            ...enableTestState,
-            canGenerateHints: [false, false, false, true, true, true]
+        expect(
+            gameReducer(enableTestState, enableGeneration([3, 4, 5]))
+        ).toMatchObject({
+            canGenerateHints: [false, false, false, true, true, true],
         });
     });
     it('should enable hint generation for all letters if all are passed', async () => {
-        expect(gameReducer(enableTestState, enableGeneration([0, 1, 2, 3, 4, 5]))).toStrictEqual({
-            ...enableTestState,
-            canGenerateHints: [true, true, true, true, true, true]
+        expect(
+            gameReducer(enableTestState, enableGeneration([0, 1, 2, 3, 4, 5]))
+        ).toMatchObject({
+            canGenerateHints: [true, true, true, true, true, true],
         });
     });
     it('should disable hint generation for letter at passed index', async () => {
-        expect(gameReducer(testState, disableGeneration(5))).toStrictEqual({
-            ...testState,
-            canGenerateHints: [true, true, true, true, true, false]
+        expect(gameReducer(testState, disableGeneration(5))).toMatchObject({
+            canGenerateHints: [true, true, true, true, true, false],
         });
     });
 });
--- frontend/src/features/game/game.slice.ts
+++ frontend/src/features/game/game.slice.ts
@@ -1,50 +1,60 @@
-import {createSlice, Draft, PayloadAction} from "@reduxjs/toolkit";
-import {decrementLetter, incrementLetter} from "./letter-utils.ts";
+import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
+import { decrementLetter, incrementLetter } from './letter-utils.ts';
 
 export interface GameState {
-    readonly letters: string[],
-    readonly canGenerateHints: boolean[]
+    readonly letters: string[];
+    readonly canGenerateHints: boolean[];
+    readonly clicks: number;
 }
 
-const initialWord = "JULINK";
+const initialWord = 'JULINK';
 export const initialGameState: GameState = {
-    letters: initialWord.split(""),
-    canGenerateHints: new Array<boolean>(initialWord.length).fill(true)
-}
+    letters: initialWord.split(''),
+    canGenerateHints: new Array<boolean>(initialWord.length).fill(true),
+    clicks: 0,
+};
 
-function change(state: Draft<GameState>, index: number, changeFunction: (letter: string) => string) {
+function change(
+    state: Draft<GameState>,
+    index: number,
+    changeFunction: (letter: string) => string
+) {
     state.letters[index] = changeFunction(state.letters[index]);
     if (index > 0)
         state.letters[index - 1] = changeFunction(state.letters[index - 1]);
     if (index < state.letters.length - 1)
         state.letters[index + 1] = changeFunction(state.letters[index + 1]);
+
+    state.clicks++;
 }
 
 export const gameSlice = createSlice({
-    name: "letters",
+    name: 'letters',
     initialState: initialGameState,
     reducers: {
-        increment: (state, {payload: index}: PayloadAction<number>) => {
+        increment: (state, { payload: index }: PayloadAction<number>) => {
             change(state, index, incrementLetter);
         },
-        decrement: (state, {payload: index}: PayloadAction<number>) => {
+        decrement: (state, { payload: index }: PayloadAction<number>) => {
             change(state, index, decrementLetter);
         },
-        enableGeneration: (state, {payload: indices}: PayloadAction<number[]>) => {
+        enableGeneration: (
+            state,
+            { payload: indices }: PayloadAction<number[]>
+        ) => {
             for (const index of indices) {
                 state.canGenerateHints[index] = true;
             }
         },
-        disableGeneration: (state, {payload: index}: PayloadAction<number>) => {
+        disableGeneration: (
+            state,
+            { payload: index }: PayloadAction<number>
+        ) => {
             state.canGenerateHints[index] = false;
         },
-    }
-})
+    },
+});
 
-export const {
-    increment,
-    decrement,
-    enableGeneration,
-    disableGeneration
-} = gameSlice.actions;
+export const { increment, decrement, enableGeneration, disableGeneration } =
+    gameSlice.actions;
 export default gameSlice.reducer;

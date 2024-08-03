248f73cea9b426d76f3c2261fa633e82ab8f9109
Julian Kim <kimjulian@grinnell.edu>
2024-08-02 23:31:51 -0500

    Fixed unit tests, removed TestLayout

--- frontend/src/features/game/letter/letter-utils.spec.ts
+++ frontend/src/features/game/letter/letter-utils.spec.ts
@@ -1,4 +1,4 @@
-import { decrementLetter, incrementLetter } from './letter-utils.ts';
+import { incrementLetter } from './letter-utils.ts';
 
 describe('incrementLetter function tests', () => {
     // TODO: wait for vitest v2 for passing expect from test context (test.for)
@@ -12,19 +12,5 @@ describe('incrementLetter function tests', () => {
     });
     it('should roll over Z to A', async () => {
         expect(incrementLetter('Z')).toStrictEqual('A');
-    });
-});
-
-describe('decrementLetter function tests', () => {
-    it.each([
-        ['B', 'A'],
-        ['C', 'B'],
-        ['E', 'D'],
-        ['Z', 'Y'],
-    ])('should increment %s to %s', async (beforeLetter, afterLetter) => {
-        expect(decrementLetter(beforeLetter)).toStrictEqual(afterLetter);
-    });
-    it('should roll over A to Z', async () => {
-        expect(decrementLetter('A')).toStrictEqual('Z');
     });
 });

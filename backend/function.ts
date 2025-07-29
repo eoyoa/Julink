c01ff19ab3f460f06afde2ee80742a54e80b365a
Julian Kim <kimjulian@grinnell.edu>
2025-07-29 16:48:15 -0500

    Update backend to use API Gateway

--- backend/function.ts
+++ backend/function.ts
@@ -1,11 +1,25 @@
-import {APIGatewayProxyResultV2, Handler} from "aws-lambda";
+import {APIGatewayEvent, APIGatewayProxyResultV2, Handler} from "aws-lambda";
 import {getWordOfTheDay} from "./get-word-of-the-day.js";
+import {BackendRequest} from "./request.js";
 
-export const handler: Handler = async (): Promise<APIGatewayProxyResultV2> => {
-    let payload = getWordOfTheDay();
+function isProperBackendRequest(request: unknown): request is BackendRequest {
+    if (!request || typeof request !== 'object') return false;
+    if (!('timestamp' in request)) return false;
+
+    return typeof request.timestamp === 'number';
+}
+
+export const handler: Handler = async ({ body }: APIGatewayEvent): Promise<APIGatewayProxyResultV2> => {
+    if (!body) throw new Error("Missing request body");
+    const request: unknown = JSON.parse(body);
+    if (!isProperBackendRequest(request)) throw new Error("Invalid request body");
+
+    const { timestamp } = request;
+
+    const payload = getWordOfTheDay();
 
     return {
         statusCode: 200,
-        body: JSON.stringify(payload)
+        body: JSON.stringify(payload),
     }
 }
